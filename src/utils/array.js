5adb7d9681a042a0e39b5f094ba96f4453307f50
David Newcomb <dnewcomb@mcclatchy.com>
2023-09-19 11:42:20 -1000

    Fixing bug with update array

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,15 +1,15 @@
 export const updateArray = (arr, elem, add) => {
   const matchIndex = arr.findIndex(x => x == elem)
-
+  console.log(elem, arr, matchIndex)
   if (add) {
-    if (matchIndex === null) {
+    if (matchIndex >= 0) {
       return arr;
     } else {
       arr = [...arr, elem];
       return arr;
     }
   } else {
-    if (matchIndex === null) {
+    if (matchIndex === -1) {
       return arr;
     } else {
       arr = arr.filter((item, i) => i !== matchIndex );
